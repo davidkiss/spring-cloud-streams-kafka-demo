202b1f166509f49172cdd6dcc3cdae187064bc3f
317959997 <david.kiss@rbc.com>
2018-01-05 11:29:59 -0500

    initial commit

---
+++ src/main/java/com/kaviddiss/streamkafka/config/StreamsConfig.java
@@ -0,0 +1,8 @@
+package com.kaviddiss.streamkafka.config;
+
+import com.kaviddiss.streamkafka.stream.GreetingsStreams;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@EnableBinding(GreetingsStreams.class)
+public class StreamsConfig {
+}
